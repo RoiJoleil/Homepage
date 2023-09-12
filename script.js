5497caea4a67d1fc7f3fc820113bd473afcac80e
Roi Joleil <97556094+RoiJoleil@users.noreply.github.com>
2023-09-12 07:53:27 +0200

    Fixing Bug causing this to not work in Chrome based Browsers

--- script.js
+++ script.js
@@ -1,79 +1,97 @@
-document.addEventListener("DOMContentLoaded", function() {
+var skillPercentage = {
+  barA: 90,
+  barB: 95,
+  barC: 80,
+  barD: 95,
+  barE: 75,
+  barF: 70,
+  barG: 100,
+  barH: 90,
+  barI: 20,
+};
 
-  var skillPercentage = {
-    barA: 90,
-    barB: 95,
-    barC: 80,
-    barD: 95,
-    barE: 75,
-    barF: 70,
-    barG: 100,
-    barH: 90,
-    barI: 20,
-  };
+function animateBar(bar) {
+  /*selects the foregroundbar element*/
+  let foregroundBar = bar.querySelector(".barColorForeground");
+  /*selects the percentage text*/
+  let percentageText = bar.querySelector(".percentage");
+  /*gets the class attribtue*/
+  let skillClass = bar.getAttribute("class");
 
-  /*selects all the g elements in #skillsChart .chart*/
-  let  skillBars = document.querySelectorAll("#skillsChart .chart > g");
-  /*the same as above*/
-  let languageBars = document.querySelectorAll("#languagesChart .chart > g");
+  let width = skillPercentage[skillClass];
+  foregroundBar.setAttribute("width", `${width}%`);
+  animateTextChange(percentageText, width, 2500);
+}
 
-  function animateBar(bar) {
-    /*selects the foregroundbar element*/
-    let foregroundBar = bar.querySelector(".barColorForeground");
-    /*selects the percentage text*/
-    let percentageText = bar.querySelector(".percentage");
-    /*gets the class attribtue*/
-    let skillClass = bar.getAttribute("class");
-
-    let width = skillPercentage[skillClass];
-    foregroundBar.setAttribute("width", `${width}%`);
-    animateTextChange(percentageText, width, 2500);
-  }
-
-  function animateTextChange(element, finalValue, duration) {
-    const startTime = Date.now();
-
-    function updateText() {
-      const currentTime = Date.now();
-      const elapsedTime = currentTime - startTime;
-
-      if (elapsedTime >= duration) {
-        element.textContent = `${finalValue}%`;
-        return;
-      }
-
-      const progress = elapsedTime / duration;
-      const interpolatedValue = Math.round(progress * finalValue);
-      element.textContent = `${interpolatedValue}%`;
-
-      requestAnimationFrame(updateText);
-    }
-
-    updateText();
-  
-  }
-
-  function createIntersectionObserver(bars){
+function createIntersectionObserver(skillBars,languageBars) {
   let options = {
     root: null,
     rootMargin: "0px",
     threshold: 0.8,
   };
 
-    bars.forEach((bar) => {
-      let observer = new IntersectionObserver ((entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            animateBar(bar);
-            observer.unobserve(entry.target);
-          }
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("INTERSECTION");
+        
+        // Assuming skillBars and languageBars are arrays of elements.
+        
+
+        for(var i = 0; i < skillBars.length; i++) {
+          animateBar(skillBars[i]);
+        }
+
+        for(var i = 0; i < languageBars.length; i++) {
+          animateBar(languageBars[i]);
+        }
+
+        /*
+        [...skillBars, ...languageBars].forEach((bar) => {
+          
+          animateBar(bar);
         });
-      },options);
-      
-      observer.observe(bar);
+        */
+
+        observer.unobserve(entry.target);
+      }
     });
+  }, options);
+
+  let mainBar = document.querySelector('#skillsChart');
+  observer.observe(mainBar);
+}
+
+function animateTextChange(element, finalValue, duration) {
+  const startTime = Date.now();
+
+  function updateText() {
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - startTime;
+
+    if (elapsedTime >= duration) {
+      element.textContent = `${finalValue}%`;
+      return;
+    }
+
+    const progress = elapsedTime / duration;
+    const interpolatedValue = Math.round(progress * finalValue);
+    element.textContent = `${interpolatedValue}%`;
+
+    requestAnimationFrame(updateText);
   }
 
-    createIntersectionObserver(skillBars);
-    createIntersectionObserver(languageBars);
-  });
+  updateText();
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  /*selects all the g elements in #skillsChart .chart*/
+  let skillBars = document.querySelectorAll("#skillsChart .chart > g");
+  /*the same as above*/
+  let languageBars = document.querySelectorAll("#languagesChart .chart > g");
+
+  createIntersectionObserver(skillBars,languageBars);
+
+});
